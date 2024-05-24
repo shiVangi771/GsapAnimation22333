84add1013fdc816476efd1b5f5d46a675d0abdb3
Shivangi Mishra <145798470+shiVangi771@users.noreply.github.com>
2024-05-24 15:48:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+ x:1200,
+    duration:2,
+    delay:1.5,
+    scale:0.5,
+    roate:360,
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true
+})
+
+gsap.to("#box2" , {
+
+   x:1200,
+    duration:2,
+    delay:3.5,
+    scale:0.5,
+    roate:360,
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true
+})
+
+gsap.to("#box3" , {
+
+    x:1200,
+    duration:2,
+    delay:5.5,
+    scale:0.5,
+    roate:360,
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true
+})
